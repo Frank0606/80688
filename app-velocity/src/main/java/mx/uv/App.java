a2dcac381c2170f84c6265e57654d152a07910c9
Francisco <frankdanielsanchez@gmail.com>
2021-10-22 18:22:22 -0500

    Proyecto velocity_casi terminado

--- app-velocity/src/main/java/mx/uv/App.java
+++ app-velocity/src/main/java/mx/uv/App.java
@@ -1,22 +1,46 @@
 package mx.uv;
+import mx.uv.Datos.Usuario;
 
 import static spark.Spark.*;
 import com.google.gson.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 public class App 
 {
+    private static Gson gson = new Gson();
+    private static Map<String, Usuario> usuarios = new HashMap<>();
+
     public static void main( String[] args )
     {
-
+        port(getHerokuAssignedPort());
         staticFiles.location("/");
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
 
         // este metodo ya no se logra ejecutar porque se ejecuta primero el location de arriba
         // Por default, abre primero un archivo que se llame index antes que cualquier otro archivo
+
+        // Cuando se ejecuta en forma de jar, se ejecuta por determinado este get, ya no el static file
         get("/", (req, res) -> "Respuesta");
         get("/hola", (req, res) -> "Hola mundo");
 
@@ -27,6 +51,18 @@ public class App
             return null;
         });
 
+        post("/usuario", (req, res) -> {
+            String payload = req.body();
+            String id = UUID.randomUUID().toString();
+            Usuario u = gson.fromJson(payload, Usuario.class);
+            u.setId(id);
+            usuarios.put(id, u);
+
+            JsonObject objetoJson = new JsonObject();
+            objetoJson.addProperty("status", "ok");
+            objetoJson.addProperty("id", id);
+            return objetoJson;
+        });
 
         // Hace que la URL no cambie
         // Representan a un archivo renderizandolo, para mantener en anonimo al recurso que se llamo
@@ -35,20 +71,29 @@ public class App
             return new VelocityTemplateEngine().render(new ModelAndView(model, "estatica.html"));
         });
 
+        // do this
         get("/velocity", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("nombre", "Lo que sea!");
-            // Esto hace que pase la llave nombre con valor lo que sea. Pasa a el archivo que ponemos, velocity.vm
-            // Pudiendo asi usar esta variable con valor lo que sea
-            model.put("apellido", "de tal");
+            model.put("nombre", "lo que sea!");
+            model.put("apellido", "de tal!");
             return new VelocityTemplateEngine().render(new ModelAndView(model, "templates/velocity.vm"));
         });
 
-        /*get("/usuarios", (req, res) -> {
+        // do this
+        get("/usuarios", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("usuarios", usaurios.values());
+            model.put("usuarios", usuarios.values());
             return new VelocityTemplateEngine().render(new ModelAndView(model, "templates/velocity.vm"));
-        });*/
+        });
 
+
+    }
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
